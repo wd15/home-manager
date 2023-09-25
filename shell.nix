9de8a7df7575c943fc55348ea7ea1b36ac4cea89
Daniel Wheeler <daniel.wheeler2@gmail.com>
2023-09-25 13:04:54 -0400

    make backup use computer hostname

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 pkgs:
 let
+  host = builtins.getEnv "HOSTNAME";
   backup = pkgs.writeShellScriptBin "backup" ''
-    rsync --delete -azvv -e ssh /home/wd15/ wd15@h190143:/vol0/laptops/wd15/
+    rsync --delete -azvv -e ssh /home/wd15/ wd15@h190143:/vol0/laptops/wd15/${host}
   '';
   vpn = pkgs.writeShellScriptBin "vpn" ''
     xdg-settings set default-web-browser google-chrome.desktop
